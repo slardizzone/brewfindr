656ff8e50508093942bb414ef599d907f850473c
slardizzone <slardizzone@gmail.com>
2014-01-03 19:25:23 -0500

    create users migration"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140103220751) do
+ActiveRecord::Schema.define(version: 20140104002227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20140103220751) do
   create_table "beers", force: true do |t|
     t.string   "name",       null: false
     t.string   "api_id",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end

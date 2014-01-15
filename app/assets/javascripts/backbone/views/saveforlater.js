cb303a1af9515e6d703d4cdcb180d71aae5613b3
slardizzone <slardizzone@gmail.com>
2014-01-15 01:27:48 -0500

    found heroku error going to push

--- app/assets/javascripts/backbone/views/saveforlater.js
+++
@@ -1,8 +0,0 @@
-     <div id="bar-1" class="bar-main-container azure">
-    <div class="wrap">
-      <div class="bar-percentage" data-percentage="<%%= this.model.get("abv") %>"></div>
-      <div class="bar-container">
-        <div class="bar"></div>
-      </div>
-    </div>
-  </div>
